08f1b6912e160d998322a9a29b7cf80908ff81f9
lidiaaire <164666946+lidiaaire@users.noreply.github.com>
2026-03-28 11:34:02 +0100

    Flowly v1.2.1 seed with server correct!

--- back/src/index.js
+++ back/src/index.js
@@ -19,7 +19,12 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Servir documentos PDF
